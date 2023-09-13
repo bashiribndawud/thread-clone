9602478e39abc0e777a3406e33bd8cb07af69fca
Bashir Dauda <56880803+bashiribndawud@users.noreply.github.com>
2023-09-13 14:21:00 +0100

    fixed activity

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,39 +1,40 @@
-import { fetchUser, getActitvity } from "@/lib/actions/user.actions";
-import { currentUser } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
-import React from "react";
 
-const page = async () => {
+import { fetchUser, getActivity } from "@/lib/actions/user.actions";
+
+async function Page() {
   const user = await currentUser();
   if (!user) return null;
 
   const userInfo = await fetchUser(user.id);
-  if (!userInfo.onboarded) redirect("/onboarding");
+  if (!userInfo?.onboarded) redirect("/onboarding");
 
-  const activities = await getActitvity(userInfo._id);
-  // console.log(userInfo._id);
+  const activity = await getActivity(userInfo._id);
+
   return (
-    <section>
-      <h1 className="head-text mb-10">Actitvity</h1>
+    <>
+      <h1 className="head-text">Activity</h1>
 
       <section className="mt-10 flex flex-col gap-5">
-        {activities?.length > 0 ? (
+        {activity.length > 0 ? (
           <>
-            {activities.length > 0 && activities.map((activity) => (
+            {activity.map((activity: any) => (
               <Link key={activity._id} href={`/thread/${activity.parentId}`}>
                 <article className="activity-card">
                   <Image
                     src={activity.author.image}
-                    alt="Profile Picture"
+                    alt="user_logo"
                     width={20}
                     height={20}
                     className="rounded-full object-cover"
                   />
                   <p className="!text-small-regular text-light-1">
-                    <span className="mr-1 text-primary-500">{activity.author.name}</span>
-                    {" "}
+                    <span className="mr-1 text-primary-500">
+                      {activity.author.name}
+                    </span>{" "}
                     replied to your thread
                   </p>
                 </article>
@@ -41,11 +42,11 @@ const page = async () => {
             ))}
           </>
         ) : (
-          <p className="!text-base-regular text-light-3">No Activity Yet</p>
+          <p className="!text-base-regular text-light-3">No activity yet</p>
         )}
       </section>
-    </section>
+    </>
   );
-};
+}
 
-export default page;
+export default Page;
